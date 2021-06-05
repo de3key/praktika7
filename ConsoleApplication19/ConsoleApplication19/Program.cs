8a4d12507593c285454f4298fb72ebcc8777a855
de3key <gnosthard@gmail.com>
2021-06-05 09:35:30 +0700

    Практика

---
+++ ConsoleApplication19/ConsoleApplication19/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] mas = new int[n];
+            Console.WriteLine("Введите число");
+            Random r = new Random();
+            int C = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(-44, 35);
+                Console.WriteLine(mas[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+
+                if (mas[i] > C)
+                {
+                    k = k + 1;
+                    s = s + mas[i];
+                    Console.WriteLine();
+                }
+                if (k == 0)
+                {
+                    Console.WriteLine("Среднее арифметическое:");
+                }
+                else
+                {
+                    Console.WriteLine("Нету таких чисел", s / k);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ ConsoleApplication20/ConsoleApplication20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] mas = new int[n];
+            Random r = new Random();
+            Console.WriteLine("Введите число");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            int s = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(1, 10);
+                Console.WriteLine(mas[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                if (k > s)
+                {
+                    Console.WriteLine("k-й элемент больше");
+                }
+                else if (s > k)
+                {
+                    Console.WriteLine("s-й элемент больше");
+                }
+                else
+                {
+                    Console.WriteLine("k-й и s-й элементы равны");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
